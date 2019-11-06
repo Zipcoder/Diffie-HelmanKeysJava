afea14f63327579309562f2a6899d579a74ee887
KrYounger <kryounger@gmail.com>
2019-11-06 11:04:10 -0500

    piano keys

---
+++ src/main/rocks/zipcode/DiffieHellman.java
@@ -0,0 +1,17 @@
+package rocks.zipcode;
+
+import java.math.BigInteger;
+
+public class DiffieHellman {
+    public BigInteger privateKey(BigInteger prime) {
+        return BigInteger.ZERO;
+    }
+
+    public BigInteger publicKey(BigInteger primeA, BigInteger primeB, BigInteger privateKey) {
+        return BigInteger.ZERO;
+    }
+
+    public BigInteger secret(BigInteger prime, BigInteger publicKey, BigInteger privateKey) {
+        return BigInteger.ZERO;
+    }
+}
